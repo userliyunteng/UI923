d018e1f312f31f4208485f60a879607926f1de7d
guzhijun369 <281878321@qq.com>
2019-09-23 09:49:14 +0800

    4444

--- config/basic_config.py
+++ config/basic_config.py
@@ -3,3 +3,4 @@ class ConfigInit:
     sendaddr_name = 'sandbox68294a9f75bc46719346fa3e801f5ad2.mailgun.org'
     sendaddr_pswd = '49d1380ac65793fe8bf4984f55c0f882-39bc661a-2812376a'
     data_filename = 'login.xls'
+    #测试测试
