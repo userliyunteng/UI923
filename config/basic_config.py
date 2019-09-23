1744f3309869b586625bcf34a8ce50077b8a1fd6
guzhijun369 <281878321@qq.com>
2019-09-23 09:51:22 +0800

    '5555'

--- config/basic_config.py
+++ config/basic_config.py
@@ -1,6 +1,6 @@
 class ConfigInit:
-    url = 'http://39.100.87.170:8090/#/login'
-    sendaddr_name = 'sandbox68294a9f75bc46719346fa3e801f5ad2.mailgun.org'
-    sendaddr_pswd = '49d1380ac65793fe8bf4984f55c0f882-39bc661a-2812376a'
+    url = 'youurl'
+    sendaddr_name = 'youstmpname'
+    sendaddr_pswd = 'youstmppw'
     data_filename = 'login.xls'
     #测试测试
